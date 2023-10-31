76404159833159bacaac5235b34af36b0b4ba000
AurelZ <zaurelien2000@gmail.com>
2023-10-31 09:38:00 +0100

    Added page name

--- partials/header.php
+++ partials/header.php
@@ -20,6 +20,18 @@
     </div>
 </div>  -->
 
+<!-- fonction pour vérifier la page où on se trouve-->
+<?php
+function getPage(): string {
+    $pageName = $_SERVER['SCRIPT_NAME'];
+    return $pageName;
+    
+}
+getPage()
+?>
+<!-- fonction pour choisir la classe à affecter -->
+
+
 <header class="header">
     <nav>
         <div class="listNav1"><a href="<?= PROJECT_FOLDER ?>index.php">Celestial Memory</a></div>
@@ -33,5 +45,26 @@
             <li><a href="<?= PROJECT_FOLDER ?>MyAccount.php" class="barColor">myAccount</a></li>
             </ul>
     </nav>
-    <div class="main-title2"><h1>CELESTIAL MEMORY</h1></div>
+    <?php
+    $test = getPage();
+    if ($test == "/memory-flash/index.php")
+        echo"<div class=\"main-title2\"><h1>accueil</h1></div>";
+    elseif ($test == "/memory-flash/games/memory/index.php")
+        echo "<div class=\"main-title2\"><h1>a vous de jouer</h1></div>";
+    elseif ($test == "/memory-flash/games/memory/score.php")
+        echo "<div class=\"main-title2\"><h1>les scores</h1></div>";
+    elseif ($test == "/memory-flash/contact.php")
+        echo "<div class=\"main-title2\"><h1>contactez nous</h1></div>";
+    elseif ($test == "/memory-flash/login.php")
+        echo "<div class=\"main-title2\"><h1>connectez vous</h1></div>";
+    elseif ($test == "/memory-flash/register.php")
+        echo "<div class=\"main-title2\"><h1>inscrivez vous vous</h1></div>";
+    else
+        echo "<div class=\"main-title2\"><h1>votre compte</h1></div>";
+    ?>
+    
+    
+    
+    
+    
 </header>
